5096be36fd39752a36833af6edfe169f093f1c6c
somratsam <somratsam2@gmail.com>
2023-06-09 22:45:46 +0600

    get all instructor data from mongodb

--- index.js
+++ index.js
@@ -12,39 +12,51 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
 
-    const classesCollection = client.db("martialDb").collection("classes");
-    app.get('/classes', async (req, res) => {
-        const result = await classesCollection.find().sort({ enrollmentCount: -1 }).limit(6).toArray();
-      res.send(result);
-    });
+        const classesCollection = client.db("martialDb").collection("classes");
+        app.get('/classes', async (req, res) => {
+            const result = await classesCollection.find().sort({ enrollmentCount: -1 }).limit(6).toArray();
+            res.send(result);
+        });
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        const instructorsCollection = client.db("martialDb").collection("instructors");
+        app.get('/instructors', async (req, res) => {
+            const result = await instructorsCollection.find().sort({ enrollmentCount: -1 }).limit(6).toArray();
+            res.send(result);
+        });
+
+        app.get('/AllInstructors', async (req, res) => {
+            const result = await instructorsCollection.find().toArray();
+            res.send(result);
+        });
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send(`Server running on port ${port}`);
+    res.send(`Server running on port ${port}`);
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
