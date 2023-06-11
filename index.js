93eed1d80d84e9cddad42f956e24814534e14586
somratsam <somratsam2@gmail.com>
2023-06-12 04:07:20 +0600

    create admin,student,instructor apis

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n1dwdgt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,27 +25,13 @@ async function run() {
     await client.connect();
 
     const classesCollection = client.db("martialDb").collection("classes");
-    const usersCollection = client.db("martialDb").collection("users");
+    const usersCollection = client.db("martialDb").collection("users")
+    const instructorsCollection = client.db("martialDb").collection("instructors");
+    const cartsCollection = client.db("martialDb").collection("carts");
 
-    // app.post('/users', async (req,res)=>{
-    //     const user = req.body
-    //     const result = await usersCollection.insertOne(user)
-    //     res.send (result)
-    // })
 
-    app.post('/users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existingUser = await usersCollection.findOne(query);
-  
-        if (existingUser) {
-          return res.send({ message: 'user already exists' })
-        }
-  
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      });
-  
+
+
 
 
 
@@ -53,37 +39,10 @@ async function run() {
       const result = await classesCollection.find().sort({ enrollmentCount: -1 }).limit(6).toArray();
       res.send(result);
     });
-
-    app.post('/classes', async (req, res) => {
-      const classData = req.body;
-      const result = await classesCollection.insertOne(classData);
-      res.status(201).send(result);
-    });
-
-    app.get('/myClasses', async (req, res) => {
-        let query = {};
-        if (req.query?.email) {
-            query = { instructorEmail: req.query.email };
-          }
-        const result = await classesCollection.find(query).toArray();
-        res.send(result);
-      });
-
-      
-
-
-     
-
-    app.put('/classes/:classId', async (req, res) => {
-      const { classId } = req.params;
-      const { feedback } = req.body;
-      const filter = { _id: classId };
-      const update = { $set: { feedback } };
-      const result = await classesCollection.updateOne(filter, update);
+    app.get('/approveClasses', async (req, res) => {
+      const result = await classesCollection.find({ status: 'approved' }).toArray();
       res.send(result);
     });
-
-    const instructorsCollection = client.db("martialDb").collection("instructors");
 
     app.get('/instructors', async (req, res) => {
       const result = await instructorsCollection.find().sort({ enrollmentCount: -1 }).limit(6).toArray();
@@ -94,6 +53,94 @@ async function run() {
       const result = await instructorsCollection.find().toArray();
       res.send(result);
     });
+
+    // instructor
+
+    app.post('/classes', async (req, res) => {
+      const classData = req.body;
+      const result = await classesCollection.insertOne(classData);
+      res.status(201).send(result);
+    });
+
+
+    app.get('/myClasses', async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { instructorEmail: req.query.email };
+      }
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // admin
+
+    app.get('/manageClasses', async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+
+
+    app.put('/classes/:classId', async (req, res) => {
+      const { classId } = req.params;
+      const { feedback } = req.body;
+      const filter = { _id: classId };
+      const update = { $set: { feedback } };
+      const result = await classesCollection.updateOne(filter, update);
+      res.send(result);
+    });
+
+
+    // student
+
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      const result = await cartsCollection.insertOne(item);
+      res.status(201).send(result);
+    });
+
+    
+
+    
+    app.get("/selectedClasses", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { studentEmail: req.query.email };
+      }
+      
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+
+    });
+
+    // Delete selected class
+    app.delete("/selectedClasses/:classId", async (req, res) => {
+      const classId = req.params.classId;
+      const query = { _id: new ObjectId (classId) };
+      
+       
+      const result = await cartsCollection.deleteOne(query)
+        res.send(result)
+     
+    });
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
