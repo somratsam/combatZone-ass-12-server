ed326ecc0eabe782133a0e6947bddd87c528ae22
somratsam <somratsam2@gmail.com>
2023-06-18 03:15:01 +0600

    fff

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function run() {
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
       res.send({ token })
     })
@@ -471,8 +471,6 @@ async function run() {
       res.send(result)
 
     });
-
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
